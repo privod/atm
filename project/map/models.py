04d714e912f5dfdbee7562507df3a860d5522f06
privod <nikolay_dvn@rambler.ru>
2017-04-14 23:03:47 +0700

    Add view and template

--- project/map/models.py
+++ project/map/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class Atm(models.Model):
     ref = models.IntegerField(primary_key=True)
-    serial = models.CharField()
-    number = models.CharField()
-    producer = models.CharField()
-    model = models.CharField()
-    city = models.CharField()
-    address = models.CharField()
+    serial = models.CharField(max_length=32)
+    number = models.CharField(max_length=32)
+    producer = models.CharField(max_length=32)
+    model = models.CharField(max_length=64)
+    city = models.CharField(max_length=64)
+    address = models.CharField(max_length=1024)
 
     def __str__(self):
         return '{}: {}'.format(self.producer, self.serial)
--- project/map/views.py
+++ project/map/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+from django.template.context import RequestContext
 
-# Create your views here.
+from map.models import Atm
+
+
+def index(request):
+    atm_list = Atm.objects.all()
+    template = loader.get_template('map/index.html')
+    context = RequestContext(request, {
+        'atm_list': atm_list,
+    })
+    return HttpResponse(template.render(context))
