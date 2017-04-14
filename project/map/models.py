a46cd962cb0df55f73844f1e7acb64f3648cad8b
privod <nikolay_dvn@rambler.ru>
2017-04-14 21:44:22 +0700

    Add model Atm

--- project/map/models.py
+++ project/map/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Atm(models.Model):
+    ref = models.IntegerField(primary_key=True)
+    serial = models.CharField()
+    number = models.CharField()
+    producer = models.CharField()
+    model = models.CharField()
+    city = models.CharField()
+    address = models.CharField()
+
+    def __str__(self):
+        return '{}: {}'.format(self.producer, self.serial)
